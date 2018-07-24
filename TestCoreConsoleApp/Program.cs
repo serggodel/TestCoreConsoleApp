79e2c3182d63061d276ec8e761e888bdf13535e1
serggodel <s.karotki@godeltech.com>
2018-07-24 11:29:25 +0300

    Add project files.

---
+++ TestCoreConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
